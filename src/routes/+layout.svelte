1bec404fa3833fbffd10dc4bea2bc9732e6260eb
Simon Manassé <simo0047@hz.nl>
2023-10-31 16:27:16 +0100

    Deleted dead space before canvas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,10 @@
   import "../app.css";
 </script>
 
-<div class="flex flex-col h-screen justify-between">
   <main>
     <slot />
   </main>
 
-  <footer>
-    <p>
-      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-    </p>
-  </footer>
-</div>
 
 <style>
 </style>
